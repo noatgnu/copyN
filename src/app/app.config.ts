4bd896dea360328b6f85e66e72f7d839133284b6
Toan Phung <toan.phung@proteo.info>
2026-02-16 13:59:38 +0000

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { PlotlyService } from 'angular-plotly.js';
+import * as PlotlyJS from 'plotly.js-dist-min';
 
 import { routes } from './app.routes';
+
+PlotlyService.setPlotly(PlotlyJS);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { ScatterPlot } from './components/scatter-plot/scatter-plot';
+import { BarChart } from './components/bar-chart/bar-chart';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'scatter-plot', component: ScatterPlot },
+  { path: 'bar-chart', component: BarChart },
+];
